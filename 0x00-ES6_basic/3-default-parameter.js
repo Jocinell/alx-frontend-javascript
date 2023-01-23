613fc17614f2a1b36d483c36f185ff1c56f7316f
ayinlaabel <kraneabel@gmail.com>
2023-01-23 21:37:31 +0100

    Task 3 updated

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,7 +1,7 @@
-export default function getSumOfHoods(
+export default getSumOfHoods = (
   initialNumber,
   expansion1989 = 89,
   expansion2019 = 19
-) {
+) => {
   return initialNumber + expansion1989 + expansion2019;
-}
+};
