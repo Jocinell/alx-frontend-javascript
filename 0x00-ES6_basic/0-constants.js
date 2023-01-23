808b2cbeeaedbb44a6132b53c9d6f97e58d61b65
ayinlaabel <kraneabel@gmail.com>
2023-01-23 13:05:49 +0100

    Task 0 updated

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,11 +1,11 @@
-function taskFirst() {
+const taskFirst = () => {
   const value = "This is for const";
 
   return value;
-}
+};
 
-function taskNext() {
+const taskNext = () => {
   let value = "This is for let";
 
   return value;
-}
+};
