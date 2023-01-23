cf8f366358832c63dbd049bdaf22bc08f1e0a0a0
ayinlaabel <kraneabel@gmail.com>
2023-01-23 22:31:04 +0100

    Task 0 updated

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,15 @@
-export default function taskFirst() {
-  const task = "I prefer const when I can.";
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
   return task;
 }
 
-export default function getLast() {
-  return " is okay";
+export function getLast() {
+  return ' is okay';
 }
 
-export default function taskNext() {
-  let combination = "But sometimes let";
+export function taskNext() {
+  let combination = 'But sometimes let';
   combination += getLast();
 
-  console.log(combination);
   return combination;
 }
