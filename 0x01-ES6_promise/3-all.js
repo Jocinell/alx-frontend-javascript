1175262b99049d2f65dab6c262efde0f6fa61f72
ayinlaabel <kraneabel@gmail.com>
2023-01-26 11:14:26 +0100

    Task 3 updated

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((value) => {
+  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
       console.log(
-        `${value[0].body} ${value[1].firstName} ${value[1].lastName}`
+        `${values[0].body} ${values[1].firstName} ${values[1].lastName}`
       );
-    })
-    .catch(() => console.log('Signup system offline'));
+    }).catch(() => console.log('Signup system offline'));
 }
